7362fd71aa7a9c3ccbf7c9f9f434456b0949a3e5
MicheleCasani <casanimichele@gmail.com>
2025-05-29 14:19:16 +0200

    [UPD] create controller and router

---
+++ router/router_film.js
@@ -0,0 +1,20 @@
+// importare express
+const express = require('express');
+
+// importo il router 
+const router = express.Router();
+
+// importo il controller
+const movieController = require("../controllers/controller_film")
+
+// creo le rotte 
+
+// index
+router.get('/', movieController.index);
+
+//show 
+router.get('/:id', movieController.show);
+
+
+// esportazione router
+module.exports = router;
